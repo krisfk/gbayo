701722d44e31095030fc4f4129b3ffb1a6cf3fac
kaiyu li <krisfk@gmail.com>
2023-07-06 03:21:31 +0800

    .

--- wp-content/themes/gbayo/index.php
+++ wp-content/themes/gbayo/index.php
@@ -118,7 +118,7 @@ get_header(); ?>
 
 </div>
 
-<div class="video-div mt-4">
+<div class="video-div mt-5">
     <img class="w-100" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/fake-video-scaled.jpg" alt="">
 </div>
 <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/5-lines-sep.png" alt=""> -->
