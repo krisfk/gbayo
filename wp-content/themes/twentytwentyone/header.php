a767da3e97ba1b591e85b6129478500719a5bfa5
kaiyu li <krisfk@gmail.com>
2023-07-05 23:29:12 +0800

    .

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -24,7 +24,7 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <div id="page" class="site">
-        <a class="skip-link screen-reader-text" href="#content">
+        <a class="skip-link screen-reader-text d-none" href="#content">
             <?php
 		/* translators: Hidden accessibility text. */
 		esc_html_e( 'Skip to content', 'twentytwentyone' );
