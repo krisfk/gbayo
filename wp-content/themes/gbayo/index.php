475f5177cc13778a40d79cc002883b3354be2fa3
kaiyu li <krisfk@gmail.com>
2023-07-06 02:17:03 +0800

    .

--- wp-content/themes/gbayo/index.php
+++ wp-content/themes/gbayo/index.php
@@ -62,7 +62,7 @@ get_header(); ?>
 
 </div>
 
-<div class="full-width-img-div position-relative">
+<div class="full-width-img-div position-relative mt-3">
 
     <div class="position-absolute top left w-100 z-1">
 
