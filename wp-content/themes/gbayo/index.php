92d62c8ec0bd585d5bdd24ffe1019100a83da59f
kaiyu li <krisfk@gmail.com>
2023-07-06 02:15:15 +0800

    .

--- wp-content/themes/gbayo/index.php
+++ wp-content/themes/gbayo/index.php
@@ -64,8 +64,13 @@ get_header(); ?>
 
 <div class="full-width-img-div position-relative">
 
-    <div class="container">
-        <img class="gbayo-big-txt" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/gbayo-big-txt.png" alt="">
+    <div class="position-absolute top left w-100 z-1">
+
+        <div class="container">
+            <img class="gbayo-big-txt" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/gbayo-big-txt.png"
+                alt="">
+        </div>
+
     </div>
     <img class="top-banner w-100" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/main-banner-scaled.jpg"
         alt="">
