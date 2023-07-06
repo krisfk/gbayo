45666b0b623f78c6ca1a715ac37b7f8e6d18f68f
kaiyu li <krisfk@gmail.com>
2023-07-06 21:14:03 +0800

    .

--- wp-content/themes/gbayo/footer.php
+++ wp-content/themes/gbayo/footer.php
@@ -20,6 +20,34 @@
 
 <footer id="colophon" class="site-footer">
 
+    <div class="container">
+
+        <div class="row">
+            <div class="col-9">
+                <table class="d-inline-block">
+                    <tr>
+                        <td class="align-top">Presented by</td>
+                        <td> <a href="javascript:void(0);" class="top-presenter-logos-a ms-4">
+
+                                <img class="" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/logo-1.png"
+                                    alt="">
+                            </a>
+                            <a href="javascript:void(0);" class="top-presenter-logos-a ms-4">
+
+                                <img src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/logo-2.png" alt="">
+                            </a>
+                            <a href="javascript:void(0);" class="top-presenter-logos-a ms-4">
+
+                                <img src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/logo-3.png" alt="">
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col-3"></div>
+        </div>
+
+    </div>
 </footer><!-- #colophon -->
 
 </div><!-- #page -->
