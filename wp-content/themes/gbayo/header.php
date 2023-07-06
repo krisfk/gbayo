02230286d6a8ed4f2331b2d970245a0057271694
kaiyu li <krisfk@gmail.com>
2023-07-07 01:01:05 +0800

    .

--- wp-content/themes/gbayo/header.php
+++ wp-content/themes/gbayo/header.php
@@ -62,7 +62,10 @@
 
         <a href="javascript:void(0);" class="close-btn"></a>
 
-        <div class="menu-top-div"></div>
+        <div class="menu-top-div">
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/gbayo-logo-white.png" alt="">
+        </div>
         <div class="lang-div">
 
             EN | 繁 | 簡
