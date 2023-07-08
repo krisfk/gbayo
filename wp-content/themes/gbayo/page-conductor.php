28ea3fbd12478f8fba4566ff052ad44c758060cf
kaiyu li <krisfk@gmail.com>
2023-07-08 18:46:45 +0800

    .

--- wp-content/themes/gbayo/page-conductor.php
+++ wp-content/themes/gbayo/page-conductor.php
@@ -38,6 +38,13 @@ get_header(); ?>
 
     <div class="row mt-5">
 
+        <div class="col-4"></div>
+        <div class="col-8">
+
+            <img class="w-100" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/conductor-img.jpg" alt="">
+
+
+        </div>
 
 
     </div>
