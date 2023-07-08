39610a4d217069269f505e0a4b968b237211ae89
kaiyu li <krisfk@gmail.com>
2023-07-08 18:19:31 +0800

    .

--- wp-content/themes/gbayo/page-presenters.php
+++ wp-content/themes/gbayo/page-presenters.php
@@ -34,82 +34,13 @@ get_header(); ?>
 
 <div class="five-lines-sep"></div>
 
-<div class="container mt-5">
 
-    <div>
-        Established and directed by The Hong Kong Academy for Performing Arts (HKAPA) with the support of the Swire
-        Group as Founding Patron, the Greater Bay Area Youth Orchestra (GBAYO) aspires to be one of the finest youth
-        orchestras in the world. It aims to provide a platform for the highest standard of classical orchestral
-        performance in the Greater Bay Area (GBA) by offering professional training and coaching by world-class
-        musicians to talented young musicians across the region.
-
-    </div>
-    <!-- <div class="text-end mt-3">
-
-        <a href="javascript:void(0);" class="more-btn">
-
-            <span>more</span>
-            <div class="five-lines-grid">
-                <div class="text-center d-block line-wrapper">
-                    <div class="line line-1"></div>
-                </div>
-                <div class="text-center d-block line-wrapper">
-                    <div class="line line-2"></div>
-                </div>
-                <div class="text-center d-block line-wrapper">
-                    <div class="line line-3"></div>
-                </div>
-                <div class="text-center d-block line-wrapper">
-                    <div class="line line-4"></div>
-                </div>
-                <div class="text-center d-block line-wrapper">
-                    <div class="line line-5"></div>
-                </div>
-
-            </div>
-        </a>
-    </div> -->
-
-</div>
 
 <div class="mt-5">
     <img class="w-100" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/about-img-2-scaled.jpg" alt="">
 </div>
 
 
-<div class="container ">
-
-    <h2 class="subtitle text-start mt-5">Management team</h2>
-
-    <div class="row mt-4">
-
-        <div class="col-4 content-fsize">
-
-            <h3 class="orange bold">Project Director</h3>
-            <div class="bold">Dr. Iñaki Sandoval</div>
-
-            Dean of the School of Music, <br>
-            Hong Kong Academy for Performing Arts
-
-        </div>
-
-        <div class="col-4 content-fsize">
-
-            <h3 class="orange bold">Artistic Coordinator</h3>
-            <div class="bold">Professor Yiu Song Lam</div>
-
-            Head of Woodwind, Brass and Percussion, <br> Hong Kong Academy For Performing Arts
-
-        </div>
-        <div class="col-4 content-fsize">
-            <h3 class="orange bold">Programme Manager</h3>
-            <div class="bold">Samantha Li</div>
-
-
-        </div>
-    </div>
-
-</div>
 
 <div class="mt-5">
     <div class="five-lines-sep"></div>
