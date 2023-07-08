62087fc3a1cd8e98cdd1b5040d3a34f44edbfc00
kaiyu li <krisfk@gmail.com>
2023-07-08 18:48:16 +0800

    .

--- wp-content/themes/gbayo/page-conductor.php
+++ wp-content/themes/gbayo/page-conductor.php
@@ -44,6 +44,43 @@ get_header(); ?>
             <img class="w-100" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/conductor-img.jpg" alt="">
 
 
+            <h2 class="subtitle text-start mt-5">Sharon Andrea Choa</h2>
+
+            <h3 class="orange">Sharon Andrea Choa</h3>
+
+
+
+
+
+
+            Born in Hong Kong and enjoyed an exceptional Anglo-Chinese education, Sharon Andrea Choa excelled in the
+            violin at an early age and first began conducting while still at school at the age of 16. She was later
+            accepted at the Royal Academy of Music as a violinist to study under the great quartet leader Sidney
+            Griller. Choa also holds a First Class B.Mus. and Ph.D degrees from King's College London. During her
+            studies at King’s, her talent for conducting was rediscovered and led to coaching from the conductors
+            Lawrence Leonard, Diego Masson, Georg Tintner, Janos Fürst, Ji í B lohlávek and Sir Colin Davis.
+            <br><br>
+            In the U.K., Choa founded Chamber Orchestra Anglia, a professional ensemble that draws together creative,
+            cultural and academic strengths in the country. Whilst there, Choa has been awarded many performance and
+            academic prizes and has been given individual grants by Arts Council England, the British Academy, the Arts
+            and Humanities Research Council and the British Council to further her many musical and entrepreneurial
+            activities. She was awarded the Norfolk Arts Award and the A.R.A.M. in 2014, the same year when she returned
+            to her birth place, Hong Kong, to take up the position as Dean of Music at The Hong Kong Academy for
+            Performing Arts (HKAPA).<br><br>
+
+            In Hong Kong, she guest conducted the Hong Kong Philharmonic Orchestra (HK Phil), and was the Principal
+            Guest conductor of the Hong Kong New Music Ensemble – taking the ensemble on tour to the Tongyeong
+            International Music Festival and Asia Cultural Center’s Performing Arts Festival in Gwangju, Korea, as well
+            as performing in a series of high-profile concerts in Hong Kong. In 2016, she and her colleagues at the
+            HKAPA founded an ensemble - Philharmonia APA - made up of the most distinguished graduates from the School
+            of Music. Since then, Choa has taken the ensemble on tour to Vienna, and performed in venues in Hong Kong to
+            considerable acclaim. In 2020, Choa joint hands with the HK Phil to develop “The Orchestra Academy Hong
+            Kong” that allows HKAPA students to receive the highest professional training. In 2022, Sharon and
+            colleagues at the HKAPA launched the “Greater Bay Area Youth Orchestra” with the Swire Group as its Founding
+            Patron.
+
+
+
         </div>
 
 
