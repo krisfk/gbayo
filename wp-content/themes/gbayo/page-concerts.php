e473ef42013b90a5538bb4fe4642d74f297b9e6b
kaiyu li <krisfk@gmail.com>
2023-07-09 23:45:01 +0800

    .

--- wp-content/themes/gbayo/page-concerts.php
+++ wp-content/themes/gbayo/page-concerts.php
@@ -64,8 +64,38 @@ get_header(); ?>
                 </tr>
             </table>
 
+        </div>
+
+
+        <div class="col-4">
+
+            <img class="w-100 concert-img"
+                src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/concert-temp-img-2.jpg" alt="">
+
+            <h3 class="bold orange mt-4">GBAYO Concert 2023 (Macau)
+            </h3>
+
+            <table class="mt-4">
+                <tr>
+                    <td class="bold pe-3 align-top">Date:</td>
+                    <td class="align-top">12 Aug 2023</td>
+                </tr>
+                <tr>
+                    <td class="bold pe-3 align-top">Time:</td>
+                    <td class="align-top">8pm</td>
+                </tr>
+                <tr>
+                    <td class="bold pe-3 align-top">Venue:</td>
+                    <td class="align-top">Macau Tower</td>
+                </tr>
+                <tr>
+                    <td class="bold pe-3 align-top">City:</td>
+                    <td class="align-top">Macau</td>
+                </tr>
+            </table>
 
         </div>
+
     </div>
 
 
