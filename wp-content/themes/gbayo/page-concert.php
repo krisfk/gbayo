2bae804c14969f8df23c6e738f951937c9296bfd
kaiyu li <krisfk@gmail.com>
2023-07-10 17:49:31 +0800

    .

--- wp-content/themes/gbayo/page-concert.php
+++ wp-content/themes/gbayo/page-concert.php
@@ -58,35 +58,7 @@ get_header(); ?>
         </div>
 
 
-        <div class="col-4 mb-5">
 
-            <img class="w-100 concert-img"
-                src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/concert-temp-img-4.jpg" alt="">
-
-            <h3 class="bold orange mt-4">GBAYO Concert 2023 <br> (Guangzhou)
-
-            </h3>
-
-            <table class="mt-4">
-                <tr>
-                    <td class="bold pe-3 align-top">Date:</td>
-                    <td class="align-top">15 Aug 2023</td>
-                </tr>
-                <tr>
-                    <td class="bold pe-3 align-top">Time:</td>
-                    <td class="align-top">8pm</td>
-                </tr>
-                <tr>
-                    <td class="bold pe-3 align-top">Venue:</td>
-                    <td class="align-top">Concert Hall, Xinghai Concert Hall</td>
-                </tr>
-                <tr>
-                    <td class="bold pe-3 align-top">City:</td>
-                    <td class="align-top">Guangzhou</td>
-                </tr>
-            </table>
-
-        </div>
 
 
     </div>
