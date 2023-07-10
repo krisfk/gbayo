e09003ecbbd11845ea8286a61b8e13411a8256ee
kaiyu li <krisfk@gmail.com>
2023-07-10 18:06:00 +0800

    .

--- wp-content/themes/gbayo/page-concert.php
+++ wp-content/themes/gbayo/page-concert.php
@@ -104,7 +104,33 @@ get_header(); ?>
                 <h3 class="bold orange d-inline-block">Li Jinzhu
 
                 </h3><span class="ms-2">Guangzhou</span>
+
+                <a href="javascript:void(0);" class="more-btn">
+
+                    <span>more</span>
+                    <div class="five-lines-grid">
+                        <div class="text-center d-block line-wrapper">
+                            <div class="line line-1"></div>
+                        </div>
+                        <div class="text-center d-block line-wrapper">
+                            <div class="line line-2"></div>
+                        </div>
+                        <div class="text-center d-block line-wrapper">
+                            <div class="line line-3"></div>
+                        </div>
+                        <div class="text-center d-block line-wrapper">
+                            <div class="line line-4"></div>
+                        </div>
+                        <div class="text-center d-block line-wrapper">
+                            <div class="line line-5"></div>
+                        </div>
+
+                    </div>
+                </a>
+
             </div>
+
+
 
 
 
