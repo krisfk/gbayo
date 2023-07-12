eb306948bc026bff714296d8d5749c9198048e16
kaiyu li <krisfk@gmail.com>
2023-07-12 15:11:19 +0800

    .

--- wp-content/themes/gbayo/page-media.php
+++ wp-content/themes/gbayo/page-media.php
@@ -38,6 +38,25 @@ get_header(); ?>
 <div class="container mt-5 content-fsize">
 
 
+    <!-- All    News    Press Releases    Videos    Photos -->
+    <ul class="news-ul">
+        <li>
+            <a href="javascript:void(0);">All</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);">News</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);">Press Releases</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);">Videos</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);">Photos</a>
+        </li>
+    </ul>
+
     <div class="row">
 
 
