27004046976e876d8186faa7ea28acb9cef476fb
kaiyu li <krisfk@gmail.com>
2023-07-13 20:13:47 +0800

    .

--- wp-content/themes/gbayo/footer.php
+++ wp-content/themes/gbayo/footer.php
@@ -101,7 +101,7 @@
             </div>
             <div class="col-lg-6 col-md-12 col-sm-12 col-12 text-lg-end text-md-start text-sm-start text-start ">
 
-                <table class="d-inline-block">
+                <table class="">
                     <tr>
                         <td class="ps-lg-5 ps-md-0 ps-sm-0 ps-0">
                             <div class="footer-logos-subtitle text-start">Co-organiser <br>
