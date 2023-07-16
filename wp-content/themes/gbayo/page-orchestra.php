9d19bbb81e2afa3c4b3a8683d3e4380b1fac6113
kaiyu li <krisfk@gmail.com>
2023-07-16 20:46:44 +0800

    .

--- wp-content/themes/gbayo/page-orchestra.php
+++ wp-content/themes/gbayo/page-orchestra.php
@@ -84,7 +84,7 @@ get_header(); ?>
         </div>
     </div>
 
-    <div class="row mt-4 gx-lg-5 gx-md-3 gx-sm-3 gx-3 ">
+    <!-- <div class="row mt-4 gx-lg-5 gx-md-3 gx-sm-3 gx-3 ">
 
         <div class="col-8 position-relative">
 
@@ -147,7 +147,7 @@ get_header(); ?>
             </a>
         </div>
     </div>
-
+ -->
 
 
 
