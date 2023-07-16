822deffe90fa0746fa43d516a8a843e19cdc6591
kaiyu li <krisfk@gmail.com>
2023-07-16 20:17:58 +0800

    .

--- wp-content/themes/gbayo/page-presenters.php
+++ wp-content/themes/gbayo/page-presenters.php
@@ -34,7 +34,7 @@ get_header(); ?>
 
 <div class="five-lines-sep"></div>
 
-<div class="container">
+<div class="container bottom-main-content">
 
     <div class="row mt-lg-5 mt-md-4 mt-sm-4 mt-4">
         <div class="col-4 ">
