544a3934160bd7ead30b3bea1922dfae30196ce3
kaiyu li <krisfk@gmail.com>
2023-07-16 19:57:36 +0800

    .

--- wp-content/themes/gbayo/footer.php
+++ wp-content/themes/gbayo/footer.php
@@ -64,7 +64,7 @@
         <div class="row mt-4">
 
             <div class="col-lg-6 col-md-12 col-sm-12 col-12  mb-lg-0 mb-md-4 mb-sm-4 mb-lg-4 mb-md-3 mb-sm-0 mb-0  ">
-                <div class="d-inline-block">
+                <div class="d-inline-block mb-3">
                     <div class="footer-logos-subtitle">Founding Patron</div>
                     <table class="d-inline-block">
                         <tr>
