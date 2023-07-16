eec305bc31ec2e2e70faceb844820789fdffcb29
kaiyu li <krisfk@gmail.com>
2023-07-16 20:56:36 +0800

    .

--- wp-content/themes/gbayo/page-orchestra.php
+++ wp-content/themes/gbayo/page-orchestra.php
@@ -68,7 +68,7 @@ get_header(); ?>
 
         <div class="col-4 position-relative">
 
-            <img class="w-100 opacity-0" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/album-1.jpg" </a>
+            <img class="w-100 " src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/album-1.jpg" </a>
         </div>
         <div class="col-8 position-relative">
 
