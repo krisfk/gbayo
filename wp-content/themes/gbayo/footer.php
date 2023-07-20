7e5a293d03e6642478d1af0beec471fc90ac2e27
kaiyu li <krisfk@gmail.com>
2023-07-21 03:52:26 +0800

    .

--- wp-content/themes/gbayo/footer.php
+++ wp-content/themes/gbayo/footer.php
@@ -297,6 +297,31 @@
                     </tr>
                 </table>
 
+                <div class="d-inline-block mb-2">
+
+                    <div class="footer-logos-subtitle" style="position:relative;top:7px">Sponsored by
+                    </div>
+                    <table class="d-inline-block">
+                        <tr>
+                            <td> <a href="javascript:void(0);" style="top:15px"
+                                    class="other-logos-a me-lg-4 me-md-2 me-sm-2 me-2">
+
+                                    <img class=""
+                                        src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-12@2x.png"
+                                        alt="">
+                                </a></td>
+
+                            <td> <a href="javascript:void(0);" style="top:15px"
+                                    class="other-logos-a me-lg-4 me-md-2 me-sm-2 me-2">
+
+                                    <img class=""
+                                        src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-13@2x.png"
+                                        alt="">
+                                </a></td>
+                        </tr>
+                    </table>
+                </div>
+
 
 
             </div>
