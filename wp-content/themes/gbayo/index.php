bd7225df82247a0e047bed4f5f1c4f1e477f214f
kaiyu li <krisfk@gmail.com>
2023-07-21 03:04:00 +0800

    .

--- wp-content/themes/gbayo/index.php
+++ wp-content/themes/gbayo/index.php
@@ -105,7 +105,7 @@ get_header(); ?>
     <h2 class="subtitle text-center mt-100">Concert</h2>
 
     <div class="row mt-4 gx-3">
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12 ">
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12 mb-30">
 
             <a href="<?php echo get_site_url();?>/concert" class="concert-a">
                 <img class="w-100" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/concert-preview-img.jpg"
