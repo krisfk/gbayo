7c09f32f61dd649e8532b72c0f4200ed21cceaec
kaiyu li <krisfk@gmail.com>
2023-07-21 04:14:59 +0800

    .

--- wp-content/themes/gbayo/footer.php
+++ wp-content/themes/gbayo/footer.php
@@ -90,29 +90,23 @@
                             <div class="footer-logos-subtitle" style="position:relative;top:7px">Sponsored by
                             </div>
                             <table class="d-inline-block">
-                                <tbody>
-                                    <tr>
-                                        <td> <a href="javascript:void(0);" style="top:15px"
-                                                class="other-logos-a me-lg-4 me-md-2 me-sm-2 me-2">
+                                <tr>
+                                    <td> <a href="javascript:void(0);" style="top:15px"
+                                            class="other-logos-a me-lg-4 me-md-2 me-sm-2 me-2">
 
-                                                <img class="lazyloaded"
-                                                    src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-12@2x.png"
-                                                    alt="" data-ll-status="loaded"><noscript><img class=""
-                                                        src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-12@2x.png"
-                                                        alt=""></noscript>
-                                            </a></td>
+                                            <img class=""
+                                                src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-12@2x.png"
+                                                alt="">
+                                        </a></td>
 
-                                        <td> <a href="javascript:void(0);" style="top:15px"
-                                                class="other-logos-a me-lg-4 me-md-2 me-sm-2 me-2">
+                                    <td> <a href="javascript:void(0);" style="top:15px"
+                                            class="other-logos-a me-lg-4 me-md-2 me-sm-2 me-2">
 
-                                                <img class="lazyloaded"
-                                                    src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-13@2x.png"
-                                                    alt="" data-ll-status="loaded"><noscript><img class=""
-                                                        src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-13@2x.png"
-                                                        alt=""></noscript>
-                                            </a></td>
-                                    </tr>
-                                </tbody>
+                                            <img class=""
+                                                src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/Asset-13@2x.png"
+                                                alt="">
+                                        </a></td>
+                                </tr>
                             </table>
                         </div>
                     </div>
