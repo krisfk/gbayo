3d79d9e9a099e08098ddbf96f618a9f5eba8e966
kaiyu li <krisfk@gmail.com>
2023-07-23 04:38:48 +0800

    .

--- wp-content/themes/gbayo/header.php
+++ wp-content/themes/gbayo/header.php
@@ -413,8 +413,9 @@ echo'</li>';
 
 
                                 <div class="row align-items-center gx-0">
-                                    <div class="col-3"> <a href="<?php echo get_site_url();?>" class="logo-a"><img src="<?php echo wp_get_attachment_image_src(get_sub_field('gbayo_logo
-'),'full')[0];?>" alt=""></a>
+                                    <div class="col-3"> <a href="<?php echo get_site_url();?>" class="logo-a"><img
+                                                src="<?php echo wp_get_attachment_image_src(get_sub_field('gbayo_logo',165),'full')[0];?>"
+                                                alt=""></a>
 
                                     </div>
 
