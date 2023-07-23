e51c25326bf14c7fe4eb9ae7c410e7d27c836289
kaiyu li <krisfk@gmail.com>
2023-07-23 17:48:08 +0800

    .

--- wp-content/themes/gbayo/page-presenters.php
+++ wp-content/themes/gbayo/page-presenters.php
@@ -41,6 +41,55 @@ get_header(); ?>
 <div class="container bottom-main-content mt-100">
 
     <div class="row mt-100 gx-3">
+
+        <?php
+    
+    if( have_rows('presenters') )
+{
+
+    while( have_rows('presenters') ) { 
+        the_row();
+        ?>
+
+        <div class="col-lg-4 col-md-12 col-sm-12 col-12  mb-lg-0 mb-md-4 mb-sm-4 mb-4 ">
+
+            <a href="javascript:void(0);" class="w-75 presenter-a">
+
+                <img class="w-75" src="http://64.227.13.14/gbayo/wp-content/uploads/2023/07/logo-1.png" alt="">
+            </a>
+
+            <h3 class="orange bold mt-lg-4  mt-md-2  mt-sm-2  mt-2"><?php echo get_sub_field('presenter_name');?></h3>
+
+            <div class="content-fsize mt-4 sponsor-para-txt sponsor-para-txt-0">
+
+                <?php echo get_sub_field('presenter_description');?>
+
+
+                <!-- The Hong Kong Academy for Performing Arts (HKAPA), established by The Hong Kong Academy for Performing
+                Arts Ordinance in 1984, is a leading tertiary institution in the performing arts in Asia. It provides
+                professional undergraduate education and practice-based postgraduate studies. The study encompasses
+                Chinese Opera, Dance, Drama, Film and Television, Music, and Theatre and Entertainment Arts. Its
+                educational philosophy reflects the cultural diversity of Hong Kong with emphasis on Chinese and Western
+                traditions, and interdisciplinary learning. <br>
+                Since 2008, the Academy has attained the Programme Area Accreditation (PAA) status awarded by the Hong
+                Kong Council for Accreditation of Academic and Vocational Qualifications (HKCAAVQ) to self-monitor and
+                accredit its own Undergraduate and Post-secondary programmes in the five programme areas of Dance,
+                Drama, Film and Television, Music, and Theatre and Entertainment Arts. Since 2016, the PAA status has
+                been extended to cover Master’s Degree programmes and below. The programmes operated by the School of
+                Chinese Opera are accredited by HKCAAVQ for recognition under the Hong Kong Qualifications
+                Framework.<br>
+                Besides the main campus in Wanchai, Béthanie, the site of the Academy’s Landmark Heritage Campus in
+                Pokfulam, has housed training facilities for the School of Film and Television since 2006.<br>
+                In the QS World University Rankings announced in 2022, the Academy is ranked 1st in Asia and 10th in the
+                world in the Performing Arts category. -->
+
+            </div>
+
+        </div><?php
+    }
+}?>
+
+        <!--     
         <div class="col-lg-4 col-md-12 col-sm-12 col-12  mb-lg-0 mb-md-4 mb-sm-4 mb-4 ">
 
             <a href="javascript:void(0);" class="w-75 presenter-a">
@@ -141,7 +190,7 @@ get_header(); ?>
                 musicians, with Lio Kuokman as the role model, laid an invaluable foundation for Macao’s musical
                 culture. Established in 1997, Macao Youth Symphony Orchestra has become Macao’s local cultural brand.
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 
