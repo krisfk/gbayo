06734dc60c8e3d46c6288cea0b04506035ce897f
kaiyu li <krisfk@gmail.com>
2023-07-23 21:10:46 +0800

    .

--- wp-content/themes/gbayo/page-artist-in-residence.php
+++ wp-content/themes/gbayo/page-artist-in-residence.php
@@ -218,7 +218,7 @@ get_header(); ?>
         <div class="col-lg-6 col-md-12 col-sm-12 col-12">
 
             <?php
-            wp_reset_postdata();
+            // wp_reset_postdata();
             wp_reset_query();
 if( have_rows('right_col_content') )
 {
