4ce110cc3ad25fc400b46319adda6a6c7b9f7734
kaiyu li <krisfk@gmail.com>
2023-07-23 15:05:42 +0800

    .

--- wp-content/themes/gbayo/header.php
+++ wp-content/themes/gbayo/header.php
@@ -386,7 +386,7 @@ echo'</li>';
 
     <div class="fix-social-elements">
         <ul class="">
-            <li class="follow-us"><?php //echo get_field('follow_us_txt',165);?></li>
+            <li class="follow-us"><?php echo get_field('follow_us_txt',165);?></li>
             <li class="dash"> ー </li>
             <li> <a target="_blank" href="javascript:void(0);" class="facebook fix-icon-a  fix-icon-a-1"> </a>
             </li>
