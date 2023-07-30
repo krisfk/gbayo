5ac0efb726ee0f66600bea0195233238bbe61ca0
kaiyu li <krisfk@gmail.com>
2023-07-31 02:06:39 +0900

    .

--- wp-content/themes/gbayo/single-training-camp.php
+++ wp-content/themes/gbayo/single-training-camp.php
@@ -87,46 +87,21 @@ while( have_rows('photo_album') ) {
     // echo 1;
 the_row();
 $idx = 0;
-if($idx%2===0)
-{
-    // echo $idx%2;
-    ?>
-    <!-- <div class="row mb-30  gx-3 "> -->
 
-    <?php
-}
 ?>
-
-
     <div class="<?php echo ($idx%2==0) ? 'col-4':'col-8' ;?> position-relative">
-
         <div class="album-foto-div position-relative">
-
             <div class="position-absolute w-100 h-100">
                 <a href="javascript:void(0);" class="album-a"
                     rel="<?php echo wp_get_attachment_image_src(get_sub_field('photo'),'full')[0];?>"
                     style="background: url(<?php echo wp_get_attachment_image_src(get_sub_field('photo'),'full')[0];?>); position: absolute; width: 100%; height: 100%; background-size: cover;"></a>
             </div>
         </div>
-
-
     </div>
-    <?php
-if($idx%2==1)
-{
-    ?>
-    <!-- </div> -->
-    <?php
-}
-
-?>
 
 
 
     <?php
-
-$idx++;
-// 
 
 }
 }
