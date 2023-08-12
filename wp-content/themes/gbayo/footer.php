f9aacfe0ff469094ebf4d0e73c6f626dacbb0738
kaiyu li <krisfk@gmail.com>
2023-08-12 18:27:04 +0800

    .

--- wp-content/themes/gbayo/footer.php
+++ wp-content/themes/gbayo/footer.php
@@ -109,14 +109,40 @@
                                 </div>
                                 <table class="d-inline-block">
                                     <tr>
-                                        <td> <a target="_blank"
-                                                href="<?php echo get_field('group_4_logo_1_link',$footer_post_id) ? get_field('group_4_logo_1_link',$footer_post_id):'javascript:void(0);';?>"
+                                        <td>
+
+
+                                            <?php 
+                                        if(get_field('group_4_logo_1_link',$footer_post_id))
+                                        {
+                                            ?>
+                                            <a target="_blank"
+                                                href="<?php echo get_field('group_4_logo_1_link',$footer_post_id);?>"
                                                 class="other-logos-a" style="top:11px">
 
                                                 <img class=""
                                                     src="<?php echo wp_get_attachment_image_src(get_field('group_4_logo_1',$footer_post_id),'full')[0];?>"
                                                     alt="">
-                                            </a></td>
+                                            </a>
+                                            <?php
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                            <div class="other-logos-a" style="top:11px">
+
+                                                <img class=""
+                                                    src="<?php echo wp_get_attachment_image_src(get_field('group_4_logo_1',$footer_post_id),'full')[0];?>"
+                                                    alt="">
+                                                </d>
+
+                                                <?php
+                                        }
+                                        
+                                        ?>
+
+
+                                        </td>
                                     </tr>
                                 </table>
                             </td>
