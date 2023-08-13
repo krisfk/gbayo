23d581c880b93b9eb220b90667ae914470d9a0bb
kaiyu li <krisfk@gmail.com>
2023-08-13 20:38:34 +0800

    .

---
+++ wp-content/themes/gbayo/page-coaches.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header(); ?>
+
+
+
+<div class="full-width-img-div position-relative">
+
+    <div class="position-absolute top left w-100 z-1">
+
+        <div class="container">
+            <h1 class="page-main-heading"><?php echo get_the_title();?></h1>
+        </div>
+
+    </div>
+    <img class="top-banner w-100"
+        src="<?php echo wp_get_attachment_image_src(get_field('top_banner_desktop'),'full')[0];?>" alt="">
+
+    <img class="top-banner w-100 mobile"
+        src="<?php echo wp_get_attachment_image_src(get_field('top_banner_mobile'),'full')[0];?>" alt="">
+</div>
+
+
+<div class="five-lines-sep"></div>
+
+<div class="container mt-100 content-fsize">
+
+    <!-- <h2 class="subtitle text-start mt-100"><?php echo get_field('management_team_title');?></h2> -->
+
+    <div class="row mt-4">
+
+
+        <div class="col-4">
+
+            <?php
+        
+        echo get_field('column_1_content');
+        ?>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+
+
+
+
+
+
+    </div>
+
+</div>
+
+<div class="mt-100">
+    <div class="five-lines-sep"></div>
+</div>
+
+
+
+<?php
+
+get_footer();
