ef49ba159a1c6a308e33e41f5175a6fe4df87ff7
kaiyu li <krisfk@gmail.com>
2023-08-13 17:49:44 +0800

    .

--- wp-content/themes/gbayo/page-soloists.php
+++ wp-content/themes/gbayo/page-soloists.php
@@ -138,7 +138,7 @@ get_header(); ?>
                 'tax_query' => array(
                     array(
                         'key' => 'show_in_soloists',
-                        'value' => '1',
+                        'value' => 'TRUE',
                         'compare' => '=',
                     )
                   )
