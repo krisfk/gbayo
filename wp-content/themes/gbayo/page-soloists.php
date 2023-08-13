7305fbfb79bd445cd841cddfa8db1d043f5d382a
kaiyu li <krisfk@gmail.com>
2023-08-13 18:29:10 +0800

    .

--- wp-content/themes/gbayo/page-soloists.php
+++ wp-content/themes/gbayo/page-soloists.php
@@ -155,7 +155,7 @@ get_header(); ?>
                 ?>
 
 
-        <div class="col-lg-4 col-md-6 col-sm-12 col-12 ">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-4">
 
             <a href="javascript:void(0);" class="content-fsize more-btn musician-more-btn d-block"
                 data-musician-name="<?php echo get_field('musician_name');?>"
