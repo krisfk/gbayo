6a4e445f0512ced8872517c8e1b80409b2cd1b96
kaiyu li <krisfk@gmail.com>
2023-08-16 23:22:16 +0800

    .

--- wp-content/themes/gbayo/page-media.php
+++ wp-content/themes/gbayo/page-media.php
@@ -74,7 +74,7 @@ get_header(); ?>
             $query_args = array(
                 'post_type' => 'media-information',
                 'orderby' => 'media_date',
-                'order' => 'DESC'
+                'order' => 'ASC'
 
             );
             
